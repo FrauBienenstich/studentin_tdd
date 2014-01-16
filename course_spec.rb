04471880788729b6f950584a7d84388cc79da135
Susanne Dewein <susanne.dewein@gmail.com>
2014-01-16 11:54:46 +0100

    Add Course class

---
+++ course_spec.rb
@@ -0,0 +1,13 @@
+require './course.rb'
+describe Course do
+  describe "initialize" do
+    before(:each) do
+      @course = Course.new("Ruby")
+    end
+
+    it "has a title" do
+      expect(@course.title).to eq("Ruby")
+    end
+
+  end
+end
