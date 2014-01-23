bfa43c1b6ad6e3f77506a8966382cfea989dfd7a
Susanne Dewein <susanne.dewein@gmail.com>
2014-01-23 16:38:39 +0100

    Implement Course
    Quite similar to Student

--- models/course.rb
+++ models/course.rb
@@ -1,8 +1,69 @@
 class Course
 
-attr_reader :title
+attr_accessor :title, :id
 
   def initialize(title)
     @title = title
+    #@id = id --> this was actually needed before @id showed up in #peristed?
   end
+
+  def persisted?
+    true if @id
+  end
+
+  def save
+    con = Course.establish_db_connection
+    persisted? ? update(con) : insert(con)
+
+  end
+
+
+  def delete
+    con = Course.establish_db_connection
+    delete_statement = con.prepare("DELETE FROM courses WHERE id = ?;")
+    delete_statement.execute @id
+  end
+
+  def self.find(needle)
+    con = establish_db_connection
+    sub_expressions = []
+    needle.each do |k, v|
+      sub_expressions << "#{k.to_s} like '%#{v.to_s}%'" #name like susanne
+    end
+
+    statement = "SELECT * FROM courses WHERE #{sub_expressions.join(' AND ')};"
+    result = con.query(statement) #sql statement
+
+    courses = []
+    result.each do |row|
+      c = Course.new(row[1])
+      c.id = row[0]
+      courses << c
+    end # courses as objects
+
+    courses
+  end
+
+
+  protected
+
+  def self.establish_db_connection
+    con = Mysql.new 'localhost', 'root', ''
+    con.query("use #{CONFIG['database']['name']};")
+    con.query("CREATE TABLE IF NOT EXISTS\
+      courses(id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(20));")
+    con
+  end
+
+  def insert(con)
+    insert_statement = con.prepare("INSERT INTO courses(title) VALUES(?);")
+    insert_statement.execute @title
+    self.id = con.insert_id
+  end
+
+  def update(con)
+    update_statement = con.prepare("UPDATE courses SET title = ? WHERE id = ?;")
+    update_statement.execute @title, @id
+  end
+
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,7 +2,19 @@ require_relative '../spec_helpers'
 require_relative '../../models/course.rb'
 
 describe Course do
+
+  def truncate_table
+    con = Course.establish_db_connection
+    con.query("TRUNCATE table courses")
+  end
+
+
+  after(:each) do
+    truncate_table
+  end
+
   describe "initialize" do
+
     before(:each) do
       @course = Course.new("Ruby")
     end
@@ -12,4 +24,93 @@ describe Course do
     end
 
   end
+
+  describe "#persisted?" do
+
+    before(:each) do
+      @course = Course.new("Ruby")
+      @course.persisted? == false
+    end
+
+    it 'is persisted with an id' do
+      @course.save
+      expect(@course.persisted?).to be_true
+    end
+
+  end
+
+  describe '#save' do
+
+    before(:each) do
+      @course = Course.new("Ruby")
+      @course.persisted? == false
+    end
+
+    it 'saves a record in the database' do
+      connection = Course.establish_db_connection
+      expect {
+        @course.save
+      }.to change{ connection.query("SELECT * from courses").num_rows}.from(0).to(1)
+    end
+
+  end
+
+  describe '#update' do
+
+    before(:each) do
+      @course = Course.new("Ruby")
+      @course.save
+      expect(@course.id).not_to be_nil 
+      @course.title = "Fuby"
+    end
+
+    it ' does not add a new course' do
+      expect {
+        @course.save
+      }.not_to change{ Course.establish_db_connection.query("SELECT * from courses").num_rows}
+    end
+
+    it 'changes a persisted entry' do
+      @course.save
+      connection = Course.establish_db_connection
+      result = connection.query("SELECT * FROM courses WHERE id = #{@course.id}")
+      expect(result.num_rows).to eq(1)
+      result.each do |row|
+        expect(row.join).to match /Fuby/
+      end
+    end
+
+  end
+
+  describe '#delete' do
+
+    before(:each) do
+      @course = Course.new("Ruby")
+      @course.save
+    end
+
+    it 'deletes a record' do
+      expect {
+        @course.delete
+      }.to change{ Course.establish_db_connection.query("SELECT * from courses").num_rows}.from(1).to(0)
+    end
+  end
+
+  describe '#find' do
+    before(:each) do
+      @course = Course.new("Ruby")
+      @course.save
+
+    end
+    it 'returns course from database' do
+      found = Course.find(:title => "Ruby")
+      expect(found[0].title).to eq("Ruby")
+      expect(found[0]).to be_a_kind_of Course
+      expect(found[0]).to be_persisted
+
+      found = Course.find(:title => "nonexistant")
+      expect(found).to be_empty
+    end
+  end
+
 end
