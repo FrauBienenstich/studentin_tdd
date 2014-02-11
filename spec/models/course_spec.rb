044c46188240cd4ce9156907e391dc7c99f561c8
Susanne Dewein <susanne.dewein@gmail.com>
2014-02-11 15:00:02 +0100

    remove comments

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -34,49 +34,4 @@ describe Course do
 
   end
 
-  # describe '#update' do
-
-  #   before(:each) do
-  #     @course = Course.new("Ruby")
-  #     @course.save
-  #     expect(@course.id).not_to be_nil 
-  #     @course.title = "Fuby"
-  #   end
-
-  #   it ' does not add a new course' do
-  #     expect {
-  #       @course.save
-  #     }.not_to change{ Course.establish_db_connection.query("SELECT * from courses").num_rows}
-  #   end
-
-  #   it 'changes a persisted entry' do
-  #     @course.save
-  #     connection = Course.establish_db_connection
-  #     result = connection.query("SELECT * FROM courses WHERE id = #{@course.id}")
-  #     expect(result.num_rows).to eq(1)
-  #     result.each do |row|
-  #       expect(row.join).to match /Fuby/
-  #     end
-  #   end
-
-  # end
-
-
-  # describe '#find' do
-  #   before(:each) do
-  #     @course = Course.new("Ruby")
-  #     @course.save
-
-  #   end
-  #   it 'returns course from database' do
-  #     found = Course.find(:title => "Ruby")
-  #     expect(found[0].title).to eq("Ruby")
-  #     expect(found[0]).to be_a_kind_of Course
-  #     expect(found[0]).to be_persisted
-
-  #     found = Course.find(:title => "nonexistant")
-  #     expect(found).to be_empty
-  #   end
-  # end
-
 end
